9647e382803de4ff54e335c0356e6380cb0f86e3
killu1 <maincharac1@gmail.com>
2026-03-13 10:21:35 +0300

    modify slog test

--- analyzer/testdata/src/loggers/simple_slog.go
+++ analyzer/testdata/src/loggers/simple_slog.go
@@ -7,13 +7,25 @@ import (
 )
 
 func log() {
-	// Тест на спецсимволы:
-	slog.Debug("start linter!") // want "log msg shouldn't have specifical symbols and emojis"
+	// Test Upper case:
+	slog.Debug("Test") // want "first symbol of log msg shouldn't be in upper case"
 
 	l := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	// Тест на Upper case:
-	l.Info("Test1", "234") // want "first symbol of log msg shouldn't be in upper case"
+	// Test english case:
+	l.Info("тест на английский язык") // want "log msg must be in english only"
 
-	// Тест на проверку множества аргументов и чувствительные данные (смотреть в файлы types.go и banwords.txt):
-	l.InfoContext(context.Background(), "zasoV1!2_-", "authtoken") // want "log msg shouldn't have specifical symbols and emojis"
+	// Test specifical symbols and emojis case
+	l.DebugContext(context.Background(), "zasoV1!2_-") // want "log msg shouldn't have specifical symbols and emojis"
+
+	// Test sensitive data case
+	const apiKey = "dpsdpqrwoiwqir2o1r21rpsfod"
+	l.WarnContext(context.Background(), "check apikey"+apiKey) // want "args shouldn't be had sensitive data: apiKey"
+
+	//Test linter checking args case
+	l.Error("some msg", "тест") // want "log msg must be in english only"
+
+	l.Error("some msg", "creds") // // want "args shouldn't be had sensitive data: creds"
+
+	l.InfoContext(context.Background(), "hi", "user", "😅") // want "log msg shouldn't have specifical symbols and emojis"
+
 }
